85eaa76cb7a596d0df0967cad265d2edf05602b5
Denly <007623@gmail.com>
2016-09-30 18:37:52 -0400

    init materialize and nav bar

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -5,31 +5,31 @@ import Task from '../components/Task.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
-
+  componentDidMount() {
+    $(".button-collapse").sideNav();
+  }
 
   render() {
     return (
       <div className="container">
         <nav>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><Link to="Landing">Landing</Link></li>
-              <li><Link to="Gallery">Gallery</Link></li>
+            <a href="#!" className="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="Landing#">Landing</Link></li>
+              <li><Link to="Discover">Gallery</Link></li>
+              <li><Link to="Message">Message</Link></li>
+              <li><Link to="Profile">My Profile</Link></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><Link to="Landing#">Landing</Link></li>
+              <li><Link to="Discover">Gallery</Link></li>
               <li><Link to="Message">Message</Link></li>
               <li><Link to="Profile">My Profile</Link></li>
             </ul>
           </div>
         </nav>
-        <header>
-          <h1>Header</h1>
-            <ul className="header">
-              <li><Link to="Landing">Landing</Link></li>
-              <li><Link to="Gallery">Gallery</Link></li>
-              <li><Link to="Message">Message</Link></li>
-              <li><Link to="Profile">My Profile</Link></li>
-            </ul>
-        </header>
 
 
         <div className="content">
