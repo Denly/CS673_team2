d98908ad7540233557adea038b2913469f49c158
Denly <007623@gmail.com>
2016-10-03 02:45:28 -0400

    init profile_card data

--- imports/ui/components/profile_card.jsx
+++ imports/ui/components/profile_card.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 export default class ProfileCard extends Component {
+  imgLink(){
+
+    var domain = "https://graph.facebook.com/v2.7/";
+    var query = "/picture?fields=picture&height=300&width=300&redirect=true";
+    var imgLink = domain + this.props.facebookID + query;
+    console.log(imgLink);
+    return imgLink;
+  }
    render() {
       return (
         <div className="card">
           <div className="card-image waves-effect waves-block waves-light">
-            <img src="/lena.jpg" />
+            <img src={this.imgLink()} />
+
           </div>
           <div className="card-content">
             <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
--- imports/ui/pages/Discover.jsx
+++ imports/ui/pages/Discover.jsx
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom'
 import ProfileCard from '../components/profile_card.jsx';
+import { createContainer } from 'meteor/react-meteor-data';
 
 export default class Discover extends Component {
   render() {
     return (
       <div>
         <h1>Discover Page</h1>
-        <ProfileCard />
-        <ProfileCard />
+        <ProfileCard
+          key = {"1234"}
+          facebookID = {"699173800246359"}
+          size = {""}
+          />
       </div>
     )
   }
 }
+
+//mapping json array to dom formate, put Collection to this.props
+export default createContainer(() => {
+  Meteor.subscribe('discoverUsers');
+
+  return {
+    users: Meteor.users.find({}).fetch(),
+    currentUser: Meteor.user(),
+  };
+}, Discover);
