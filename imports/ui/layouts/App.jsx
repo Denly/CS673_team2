6d84c2bf91741903aaa699d060f921c4b3e5d034
Denly <007623@gmail.com>
2016-10-16 18:29:36 -0400

    messageRoom popout

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -4,9 +4,17 @@ import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 // App component - represents the whole app
 export default class App extends Component {
   componentDidMount() {
-    //materialize button-collapse inital
-    $(".button-collapse").sideNav();
-
+    //materialize sideNav inital
+    $(".button-collapse").sideNav({
+      closeOnClick: true // Closes side-nav on <a> clicks
+    }
+    );
+    $('.message-room').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right',  // Choose the horizontal origin
+    }
+    );
+      
     //FB inital
     window.fbAsyncInit = function() {
       FB.init({
@@ -24,6 +32,11 @@ export default class App extends Component {
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   }
+    
+  //pop out message room
+    popMessageRoom(){
+    $('.message-room').sideNav('show');
+  }
 
   render() {
     return (
@@ -32,23 +45,54 @@ export default class App extends Component {
           <div className="nav-wrapper">
             <a href="#!" className="brand-logo"><img src="/logo_s.svg"/>MeetCute</a>
             <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+        
             <ul className="right hide-on-med-and-down">
               <li><Link to="Landing#">Landing</Link></li>
               <li><Link to="Discover">Discover</Link></li>
-              <li><Link to="Message">Message</Link></li>
+              <li><Link
+                to="Message"
+                onClick={this.popMessageRoom.bind(this)}
+                >Message</Link></li>
               <li><Link to="Profile">My Profile</Link></li>
               <li><Link to="Document">Document</Link></li>
             </ul>
+              
             <ul className="side-nav" id="mobile-demo">
               <li><Link to="Landing#">Landing</Link></li>
               <li><Link to="Discover">Discover</Link></li>
-              <li><Link to="Message">Message</Link></li>
+              <li><Link 
+                to="Message"
+                onClick={this.popMessageRoom.bind(this)}
+                >Message</Link></li>
               <li><Link to="Profile">My Profile</Link></li>
               <li><Link to="Document">Document</Link></li>
             </ul>
           </div>
         </nav>
-
+        <div>
+        </div>
+        
+        <a href="#" data-activates="slide-out" className="message-room"></a>
+        <ul id="slide-out" className="side-nav">
+            <li><div className="userView">
+            <a className="waves-effect" href="#!">
+                <span>Username</span> <span> MessageRoom</span>
+            </a>
+            </div>
+            </li>
+            <li><div className="userView">
+            <a className="waves-effect" href="#!">
+                <span>Username</span> <span> MessageRoom</span>
+            </a>
+            </div>
+            </li>
+            <li><div className="userView">
+            <a className="waves-effect" href="#!">
+                <span>Username</span> <span> MessageRoom</span>
+            </a>
+            </div>
+            </li>
+        </ul>
 
         <div className="container">
           {this.props.children}
