543a5e7cf564e7390ebcb3d8f3d400204d82cf2e
Becky <mostly.becky@gmail.com>
2016-10-16 16:18:37 -0400

    [BB] Add self-introduction content area to profile page

--- imports/ui/components/profile_card.jsx
+++ imports/ui/components/profile_card.jsx
@@ -3,13 +3,16 @@ import React, { Component, PropTypes } from 'react';
 export default class ProfileCard extends Component {
   render() {
     return (
-      <div className="card">
+      <div className="card profile-photo">
         <div className="card-image waves-effect waves-block waves-light">
           <img src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
         </div>
-        <div className="card-content">
+        <div className="card-content profile-name">
           <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
           <p><a href="#">This is a link</a></p>
+        </div>
+        <div className="card-content profile-intro">
+          This is my self-introduction yayyyayayayy.
         </div>
       </div>
     )
