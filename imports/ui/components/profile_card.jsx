d67058bc032eb8a80b62e9bbd43aa066c55821fb
Becky <mostly.becky@gmail.com>
2016-10-23 15:29:16 -0400

    Better placeholder text for now

--- imports/ui/components/profile_card.jsx
+++ imports/ui/components/profile_card.jsx
@@ -8,11 +8,11 @@ export default class ProfileCard extends Component {
           <img src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
         </div>
         <div className="card-content profile-name">
-          <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-          <p><a href="#">This is a link</a></p>
+          <span className="card-title grey-text text-darken-4">User Name<i className="material-icons right">more_vert</i></span>
+          <p><a href="#">Edit Profile</a></p>
         </div>
         <div className="card-content profile-intro">
-          This is my self-introduction yayyyayayayy.
+          Hello! I've never made an online dating profile before so I don't know what to write!
         </div>
       </div>
     )
