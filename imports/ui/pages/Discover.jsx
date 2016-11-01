e9e1a51a75e75405361838715e1bcded368f1c00
Becky <mostly.becky@gmail.com>
2016-10-31 20:13:00 -0400

    Finish hacky fix for filtering user on Discover and only displaying current user on Profile

--- imports/ui/pages/Discover.jsx
+++ imports/ui/pages/Discover.jsx
@@ -3,11 +3,11 @@ import ProfileCard from '../components/profile_card.jsx';
 import { createContainer } from 'meteor/react-meteor-data';
 
 export default class Discover extends Component {
-  renderProfileCardList() {
+  renderDiscoverCardList() {
 
     return this.props.users.map((user) => {
       console.log(user);
-      if (user._id != Meteor.user()._id)
+      if (user._id != Meteor.user()._id)  //super hacky, needs refactor
         {return (
               <ProfileCard
                 key = {user._id}
@@ -20,7 +20,7 @@ export default class Discover extends Component {
     return (
       <div>
         <h1>Discover Page</h1>
-        {this.renderProfileCardList()}
+        {this.renderDiscoverCardList()}
       </div>
     )
   }
@@ -29,7 +29,7 @@ export default class Discover extends Component {
 //need to rewrite this to NOT user current user and instead use
 //test users from database
 
-//mapping json array to dom formate, put Collection to this.props
+//mapping json array to dom format, put Collection to this.props
 export default createContainer(() => {
   Meteor.subscribe('discoverUsers');
 
@@ -38,7 +38,6 @@ export default createContainer(() => {
       return ({_id: user._id,
       imgSrc: "https://graph.facebook.com/v2.7/" + user.services.facebook.id + "/picture?fields=picture&height=960&width=960&redirect=true",
     });
-    }),
-    currentUser: Meteor.user(),
+    })
   };
 }, Discover);
--- imports/ui/pages/Profile.jsx
+++ imports/ui/pages/Profile.jsx
@@ -5,25 +5,40 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 class Profile extends Component {
 
+  renderProfileCard() {
+
+    return this.props.users.map((user) => {
+      console.log(user);
+      if (user._id == Meteor.user()._id) //super hacky, needs refactor
+        {
+          return (
+              <ProfileCard
+                key = {user._id}
+                imgSrc = {user.imgSrc} />
+            );
+        }
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Profile</h1>
-        <ProfileCard
-          key = {this.props._id}
-          imgSrc = {this.props.imgSrc} />
+        <h1>Profile Page</h1>
+        {this.renderProfileCard()}
       </div>
     )
   }
 }
 
+//mapping json array to dom format, put Collection to this.props
 export default createContainer(() => {
+  Meteor.subscribe('discoverUsers');
+
   return {
-   user: Meteor.users.find({}).fetch().map((user) => {
-    return ({_id: user._id,
-    imgSrc: "https://graph.facebook.com/v2.7/" + user.services.facebook.id + "/picture?fields=picture&height=960&width=960&redirect=true",
-  });
-  }),
-  currentUser: Meteor.user(),
-};
+    users: Meteor.users.find({}).fetch().map((user) => {
+      return ({_id: user._id,
+      imgSrc: "https://graph.facebook.com/v2.7/" + user.services.facebook.id + "/picture?fields=picture&height=960&width=960&redirect=true",
+    });
+    })
+  };
 }, Profile);
