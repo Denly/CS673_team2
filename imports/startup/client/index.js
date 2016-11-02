e7b76b7092e99e56862f9460e8db72571687a3a7
Gautam Bhat <gautambhat@wireless1x-168-122-11-49.bu.edu>
2016-11-02 14:05:35 -0400

    Not sure

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,6 +1,7 @@
-import { Messages } from '/imports/api/message/messages.js';
+/*import { Messages } from '/imports/api/message/messages.js';
 import { MessageRooms } from '/imports/api/message/messageRooms.js';
 import { Images } from '/imports/api/image/images.js';
+*/
 import { Control } from '/imports/api/control/control.js';
 
 //put Controllers in gobal namespace
