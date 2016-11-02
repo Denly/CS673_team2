3b10052660a5859d1a74653bf8af5351bcbacf65
anisa93 <nisa.adiba@gmail.com>
2016-11-02 15:07:40 -0400

    made profile crad smaller in discover

--- imports/ui/components/message_rooms_poput.test.jsx
+++ imports/ui/components/message_rooms_poput.test.jsx
@@ -2,10 +2,10 @@ import { Factory } from 'meteor/dburles:factory';
 import React from 'react';
 import { shallow } from 'enzyme';
 import { chai } from 'meteor/practicalmeteor:chai';
-import MessageRoomCard from './message_room_card.jsx';
+import MessageRoomPopout from './message_rooms_popout.jsx';
 
-describe('MessageRoomCard', () => {
-  it('should render data in MessageRoomCard', () => {
+describe('MessageRoomPopout', () => {
+  it('should render data in MessageRoomPopout', () => {
     const msgRoom = {
       id: Math.random(),
       imgSrc: 'img_not_find.jpg',
@@ -13,9 +13,19 @@ describe('MessageRoomCard', () => {
       name: 'name',
       date: '3/9 2017',
     };
-    const MRC = shallow(<MessageRoomCard {...msgRoom} />);
+	const msgRoom1 = {
+      id: Math.random(),
+      imgSrc: 'img_not_find.jpg',
+      message: 'message1..',
+      name: 'name1',
+      date: '4/9 2017',
+    };
+	const msgRooms = [msgRoom, msgRoom1]
+    const MRC = shallow(<MessageRoomPopout messageRooms = {msgRooms} />);
     chai.assert(MRC.text().includes(msgRoom.message));
     chai.assert(MRC.text().includes(msgRoom.name));
     chai.assert(MRC.text().includes(msgRoom.date));
+	//chai.assert(MRC.text().includes(msgRoom.imgSrc))
+	
   });
 });
--- imports/ui/components/profile_card.jsx
+++ imports/ui/components/profile_card.jsx
@@ -1,11 +1,11 @@
-import React, { Component, PropTypes } from 'react';
+ import React, { Component, PropTypes } from 'react';
 
 export default class ProfileCard extends Component {
   render() {
     return (
-      <div className="card profile-photo">
-        <div className="card-image waves-effect waves-block waves-light">
-          <img src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
+        <div className="card profile-photo">
+        <div className="brand-discoverImage">
+		   <img src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
         </div>
         <div className="card-content profile-name">
           <span className="card-title grey-text text-darken-4">User Name<i className="material-icons right">more_vert</i></span>
