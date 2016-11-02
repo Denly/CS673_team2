7eb30299cdf3c42985ba71c73dff9304b3fa51dd
Denly <007623@gmail.com>
2016-11-02 14:23:10 -0400

    working on connect data

--- imports/ui/pages/Message.jsx
+++ imports/ui/pages/Message.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import MessageRoomCard from '../components/message_room_card.jsx';
 import { createContainer } from 'meteor/react-meteor-data';
 
+
 export default class Message extends Component {
   render() {
     return (
       <div>
+
         <h1><img width="150" height="150" src={'img_not_find.jpg'}alt="" className="circle"/> Name</h1>
-
         <ul className="collection">
-
           {/*right msg*/}
           <li className="collection-item avatar right-align">
             <span className="title">{'date'}</span>
@@ -23,7 +23,23 @@ export default class Message extends Component {
         </li>
 
       </ul>
+      <div className="row">
+          <div className="input-field col s10">
+            <textarea id="textarea1" className="materialize-textarea"></textarea>
+            </div>
+            <div className="input-field col s2">
+            <i className="material-icons">send</i>
+          </div>
+        </div>
     </div>
   )
 }
 }
+
+export default createContainer(() => {
+
+
+  return {
+
+  };
+}, Message);
