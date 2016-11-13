b17c6511cf3acfa23a6bcd5f976fcf713d092032
Denly <007623@gmail.com>
2016-11-13 16:44:54 -0500

    connect msgRoom to msg

--- imports/api/control/control.js
+++ imports/api/control/control.js
@@ -7,30 +7,30 @@ import { Images } from '/imports/api/image/images.js';
 
 
 const _clientSendMessage = function( toUserId, text ){
-
+  // new msg
   var MsgId =  Messages.insert({
     fromUserId: Meteor.userId(),
     toUserId: toUserId,
     text: text,
-    createdAt: Date(),
+    createdAt: new Date(),
   });
 
   if(!MsgId){
     console.error('insert msg failed');
     return 0;
   }
-
+  // new msgRoom
   var msgRoom = MessageRooms.findOne({fromUserId: Meteor.userId(), toUserId: toUserId});
   if( msgRoom ) {
       var MsgRoomSuccess = MessageRooms.update(msgRoom._id,
-      {$set: {text: text, updatedAt: Date()}}
+      {$set: {text: text, updatedAt: new Date()}}
     );
   }else{
     var MsgRoomSuccess = MessageRooms.insert({
       fromUserId: Meteor.userId(),
       toUserId: toUserId,
       text: text,
-      createdAt: Date(),
+      createdAt: new Date(),
     });
   }
 
@@ -41,16 +41,20 @@ const _clientSendMessage = function( toUserId, text ){
   }
 
   if(MsgId && MsgRoomSuccess){
-    console.log('MsgId:',MsgId, ', MsgRoomSuccess:',MsgRoomSuccess )
+    console.log('Successed, MsgId:',MsgId, ', MsgRoomSuccess:',MsgRoomSuccess )
   }
-
 }
+
+const _clientGetLatestMsg = function(toUserId){
+  return Messages.findOne({fromUserId: Meteor.userId(), toUserId: toUserId}, {sort: {createdAt: -1}});
+};
 
 const _clientEditProfile = function(){
   console.log('_clientEditProfile');
-}
+};
 
 export const Control = {
   clientSendMessage: _clientSendMessage,
   clientEditProfile: _clientEditProfile,
+  clientGetLatestMsg: _clientGetLatestMsg,
 }
--- imports/ui/components/message_rooms_popout.jsx
+++ imports/ui/components/message_rooms_popout.jsx
@@ -4,13 +4,13 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import { MessageRooms } from '/imports/api/message/messageRooms.js';
 import { Messages } from '/imports/api/message/messages.js';
+import { Control } from '/imports/api/control/control.js';
 
 class MessageRoomsPopout extends Component {
 
   render() {
     return (
       <ul id="slide-out" className="side-nav collection">
-        <li><Link to="/Landing">Landing</Link></li>
         {this.props.messageRooms.map((o) => (
            <MessageRoomCard
             key = {o.id}
@@ -34,12 +34,15 @@ export default createContainer(() => {
 //subscribe messageRooms here
 
   return {messageRooms: MessageRooms.find().fetch().map((mr)=>{
+    var msg = Control.clientGetLatestMsg(mr.toUserId);
+    toUser = Meteor.users.findOne(mr.toUserId)
+    name = toUser ? toUser.name : 'name';
     return {
       id: mr._id,
       imgSrc: '/img_not_find.jpg', // '/' is start with root url, without it, is become http://localhost:3000/Message/<sth>/xx.jpg which is not we want
-      message: 'message..',
-      name: 'name',
-      date: '3/9 2017',
+      message: msg.text,
+      name: name,
+      date: msg.createdAt.toDateString(),
     };
   })}
 
