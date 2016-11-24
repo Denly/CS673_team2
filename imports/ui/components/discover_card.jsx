d656ab3ab6b785b7d6c6a4248b99cec76d4f7671
anisa93 <nisa.adiba@gmail.com>
2016-11-23 22:41:54 -0500

    updated to discover grids in discover page

--- imports/ui/components/discover_card.jsx
+++ imports/ui/components/discover_card.jsx
@@ -2,12 +2,12 @@ import React, { Component, PropTypes } from 'react';
 
 export default class DiscoverCard extends Component {
   render() {
+    console.log('step 1');
     return (
-      <div className="card profile-photo">
-        <div className="card-image waves-effect waves-block waves-light">
-          <img class="responsive-img" src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
-        </div>
-
+     <div className="card profile-photo" >
+	 <div className="brand-discoverImage">
+          <img src= {this.props.imgSrc ? this.props.imgSrc : "img_not_find.jpg"} />
+       </div>
         <div className="card-content profile-name">
           <label>Name</label>
           <div>
