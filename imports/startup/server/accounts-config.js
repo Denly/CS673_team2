ae778faae417c601b5cda9a2000a112b193d894c
anisa93 <nisa.adiba@gmail.com>
2016-12-04 14:10:56 -0500

    uhhh

--- imports/startup/server/accounts-config.js
+++ imports/startup/server/accounts-config.js
@@ -1,10 +1,11 @@
+//Your service which we are trying to connect is facebook
 ServiceConfiguration.configurations.update(
       { "service": "facebook" },
       {
         $set: {
-          "appId": "303809510001753",
-          "secret": "be6ca26e6766d7820b889b78d9d4143a"
+          "appId": "303809510001753",//Corresponding app ID to connect to facebook
+          "secret": "be6ca26e6766d7820b889b78d9d4143a"//corresponding secret key to connect to facebook
         }
       },
-      { upsert: true }
+      { upsert: true }//if rows already exist update them else add them
     );
