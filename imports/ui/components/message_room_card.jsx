e1d957c529b795e7addef7337f576f2388710db7
Gautam Bhat <gautambhat@dhcp-wifi-8021x-155-41-98-30.bu.edu>
2016-12-11 15:43:13 -0500

    Added emojis to messaging

--- imports/ui/components/message_room_card.jsx
+++ imports/ui/components/message_room_card.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
+import {emojify} from 'react-emojione';
 
 export default class MessageRoomCard extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class MessageRoomCard extends Component {
           <Link to={'/Message/'+this.props.toUserId} className="collection-item avatar waves-effect">
           <img src={this.props.imgSrc} alt="" className="circle"/>
           <span className="title">{this.props.name}</span>
-          <p>{this.props.message}<br/>{this.props.date}
+          <p>{emojify(this.props.message, {output: 'unicode'})}<br/>{this.props.date}
           </p>
           <span className="secondary-content">
               <i className="material-icons">grade</i>
