bbd0cd807794a03b618dd1bb28c203d78bddd2ad
Gautam Bhat <gautambhat@Gautams-MacBook-Pro.local>
2016-12-11 01:31:11 -0500

    Added Emoji Support

--- imports/ui/pages/Landing.jsx
+++ imports/ui/pages/Landing.jsx
@@ -1,16 +1,34 @@
+import {emojify} from 'react-emojione';
 import React, { Component } from 'react';
 import Blaze from 'meteor/gadicc:blaze-react-component';
+
+
+const checkString = emojify('MeetCute - A Modern Dating App :wink: :) :beers:', {output: 'unicode'});
+const options = {
+    convertShortnames: true,
+    convertUnicode: true,
+    convertAscii: true,
+    styles: {
+        backgroundImage: 'url(emojione.sprites.png)',
+        width: '32px',
+        height: '32px',
+        margin: '4px'
+    },
+    // this click handler will be set on every emoji
+    handleClick: event => alert(event.target.title)
+};
 
 export default class Landing extends Component {
   logout(){
     AccountsTemplates.logout();
     console.log("log out");
   }
+  
 
   render() {
     return (
       <div>
-        <h1>Landing Page</h1>
+        <h1>{checkString}</h1>
         <Blaze template="atForm" />
         {Meteor.user() ? '' :
         <a className="waves-effect waves-light btn" onClick={this.logout}><i className="material-icons left">power_settings_new</i>Logout</a>
